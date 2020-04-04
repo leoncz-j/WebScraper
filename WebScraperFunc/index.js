1abf22570898cdbb4c334231dea491710598f773
leoncz-j <61799630+leoncz-j@users.noreply.github.com>
2020-04-04 19:16:24 +0200

    upload files

---
+++ WebScraperFunc/index.js
@@ -0,0 +1,52 @@
+const JSDOM = require('jsdom').JSDOM;
+const readability = require("readability-nodejs");
+const axios = require('axios');
+
+
+module.exports = async function (context, req) {
+
+  if (req.query.url || (req.body && req.body.url)) {
+      var url = req.body.url || req.query.url;
+
+      try {
+        // get html
+          await axios.get(url)
+          .then((response) => {
+              if(response.status === 200) {
+              var html = response.data;
+                
+              var dom = new JSDOM(html,{url:url});
+
+              // firefox readabiliy
+              let reader = new readability.Readability(dom.window.document);
+              let article = reader.parse();
+                // response
+              context.res = {
+
+                body : {
+                   "url": url,
+                   "title":article.title,
+                  "text":article.textContent
+                }
+
+              };
+              }
+          }, (error) => context.log(error) );
+      
+         
+        } catch (error) {
+          context.log(error);
+          }
+
+  } else{
+
+    context.res = {
+      status: 400,
+      body: "Please pass an url on the query string or in the request body"
+    };
+
+  }
+
+context.done();
+          
+};
